8f999ec276f2c354d409ea404fefb4aa4e1305c3
Gaurav Avhale <126567166+gauravavhale@users.noreply.github.com>
2024-06-01 21:43:16 +0530

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 
 export const Footer = () => {
   return (
-    <div className={`position-fixed bottom-0 w-100 bg-primary text-center text-white ${styles.footer}`}>
+    <div className={`position-fixed bottom-0 w-100 bg-primary text-center text-aqua ${styles.footer}`}>
        &copy; rights belongs to me
     </div>
   )
